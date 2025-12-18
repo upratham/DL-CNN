46820000d15f4aaa7f9d336a98cb75a4009b4733
PRATHAMESH SUHAS URAVANE <upratham2002@gmail.com>
2025-12-18 00:07:21 -0500

    data preprocessingn implemented with resize and label encoding

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,10 +1,41 @@
 import os;import re;import numpy as np
-from pathlib import path 
+import glob
 from sklearn.model_selection import train_test_split
 import tensorflow as tf 
+from tqdm import tqdm 
+import cv2
 
-def data_preprocess(img_size)
+def data_preprocess(img_size,data_dir):
+    label_names = ["bougainvillea","daisies","garden_roses","gardenias","hibiscus","hydrangeas","lilies","orchids","peonies","tulip"]
+    Images= []
+    labels= []
+    label_idx=[]
+
+    # --------------label encoding and image resize -------------# 
+    for f in tqdm(glob.glob(data_dir+'\*.jpg')):
+        #split on first number and remove last character
+        name = re.split(r"\d", os.path.splitext(os.path.basename(f))[0], 1)[0][:-1] 
+        idx=label_names.index(name)
+        image = cv2.imread(f)
+        image = cv2.resize(image,img_size)
+        labels.append(name)
+        Images.append(image)
+        label_idx.append(idx)
+        # print("\nExtracting")
+        # print(name)
+        # print(idx)
+        # #print(image)
+    return Images, labels, label_idx
+
+
+
+
+
+
+
+
     
+
 
 
 
@@ -12,7 +43,13 @@ def data_preprocess(img_size)
 
 def main():
     img_size=(200,200)
-    data_preprocess(img_size)
+    path=r'data/flowers'
+    Images,labels,label_idx=data_preprocess(img_size=img_size,data_dir=path)
+    Images=np.array(Images)
+    label_idx=np.array(label_idx)
+    print('img shape:',Images.shape)
+    print('label idx shape:',label_idx.shape)
+    print('label length:',len(labels))
 
 if __name__== "__main__":
     main()
