d0687640e125205324e10416666b54fc5979227a
PRATHAMESH SUHAS URAVANE <upratham2002@gmail.com>
2025-12-17 23:20:22 -0500

    data collcted manually

---
+++ src/data_preprocess.py
@@ -0,0 +1,18 @@
+import os;import re;import numpy as np
+from pathlib import path 
+from sklearn.model_selection import train_test_split
+import tensorflow as tf 
+
+def data_preprocess(img_size)
+    
+
+
+
+
+
+def main():
+    img_size=(200,200)
+    data_preprocess(img_size)
+
+if __name__== "__main__":
+    main()
